23252ce1c69fb32b257caa5904747e64115e178e
Eudis Duran <eudisd@live.com>
2011-03-24 18:18:18 -0400

    Worked a bit on k-means

--- k-means.py
+++ k-means.py
@@ -1,6 +1,19 @@
 #! /usr/bin/env python
 
+import time
+
 def main():
+
+    n = 10
+    K = 0
+    d = 0
+    P = []
+    seed(time(NULL))
+    # Generate Random Points
+    for i in range(10):
+        P.append(rand()%Range)        
+
+
     return
 
 if __name__ == "__main__":
