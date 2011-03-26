164405c6d0dea47ae1f1883614463cad1e1ad948
Eudis Duran <eudisd@live.com>
2011-03-26 18:13:53 -0400

    Fixed problem 3

--- k-means.py
+++ k-means.py
@@ -1,23 +1,42 @@
 #! /usr/bin/env python
 
-import time
+import time, random, math
 
 def main():
 
-    n = 10
-    K = 0
+    point_range = 100
+    n = 100
+
+    K = []
     d = 0
     P = []
-    seed(time(NULL))
-    # Generate Random Points
-    for i in range(10):
-        P.append(rand()%Range)        
 
+
+    random.seed(time.time())
+    # Generate Random Points
+    for i in range(n):
+        P.append(random.randint(0, point_range))
+
+
+    # Randomly chose K means ( len(K) < n )
+    for i in range(random.randint(0, n - 1)):
+        K.append(P[i])
+
+    print "P", P
+    print
+    print "K", K
+
+    for i in P:
+        for j in K:
+            Min = point_range
+            if ( abs( i - j ) < Min ):
+               Min = abs( i - j )
 
     return
 
 if __name__ == "__main__":
     main()
+
 
 
 
