b64272e8c2e9f3ab05f0fb14e11adcc204417aa7
Eudis Duran <eudisd@live.com>
2011-04-04 23:46:55 -0400

    Made some progress with the soft k-means

--- k-means.py
+++ k-means.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import random
+import random, time
 
 class Cluster:    
     def __init__(self):
@@ -8,29 +8,61 @@ class Cluster:
         self.count = 0
         self.error = 0
 
+    
+def min_index(K, p):
+    """ Returns the index to the cluster with the
+    minimum distance"""
+    m = 1000
+    for i in range(len(K)):
+        if ( abs(K[i].mean - p) < m ):
+            m = i
+
+    return m
+def argmin(K, p):
+    """ Returns minimum distance"""
+    m = 1000
+    for i in range(len(K)):
+        if ( abs(K[i].mean - p) < m ):
+            m = abs(K[i].mean - p)
+
+    return m
 
 def main():
 
-    d = 10
+    d = 5
     
     lim = 100
-
+    random.seed(time.time())
     #This is randomly generated
     histogram = list(set([random.randint(0, lim) for i in range(256)]))
     N = len(histogram)
     
-    # Chose k random clusters
-    clusters = list(set([ histogram[ random.randint(0, lim) % N ] for i in range( histogram[ random.randint(0, N) ]) ]))
-    K = len(clusters)
-    
+    K = N/2
 
+    k = []
     # Soft Sequential K-Means
-    
-    
+    c = Cluster()
+    c.mean = histogram[0]
+    c.count += 1
+    k.append(c)
+    for i in range(1, N):
+        if( len(k) < K ):
+            
+            min_distance = argmin(k, histogram[i])
+            if ( min_distance > d ):
+                c = Cluster()
+                c.mean = histogram[i]
+                c.count += 1
+                k.append(c)
+            else:
+                index = min_index(k, histogram[i])
 
+                # Compute K means
+                pass
+             
+        
     
-    print histogram 
-    print clusters
+    
     
     return
 
